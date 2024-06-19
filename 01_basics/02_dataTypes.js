a9c63850e782ca883dac9761aac182da31a3e0c8
Aditya Powale <119852907+Aytida26@users.noreply.github.com>
2024-06-19 06:38:17 +0000

    changes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -7,3 +7,21 @@ console.log(3
     3)//code readability should be high
 
 console.log("Aditya")
+
+
+let name = "Aditya"
+let age = 23
+let isLoggedIn = true
+let state ;
+
+//number => 2 to the power of 53
+//bigint
+// string can be used in "" and ''
+//boolean => true or false
+//null  => standalone value
+//undefined =>
+//symbol=> used for unique
+
+//object
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
