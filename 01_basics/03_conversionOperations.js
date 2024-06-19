651068afcf7ce9f81240a8ae62ed3643b7121128
Aditya Powale <119852907+Aytida26@users.noreply.github.com>
2024-06-19 11:04:41 +0000

    conversion operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -34,9 +34,48 @@ let booleanIsLoggedIn = Boolean (isLoggedIn)
 
 
 let someNumber = 24
-console.log(typeof someNumber);
+// console.log(typeof someNumber);
 
 let stringNumber = String(someNumber)
 
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+
+
+/******************* Operations************************ */
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+3);
+// console.log(2-3);
+// console.log(2*3);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Aditya"
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log( 2 + 2  + "1");
+// console.log( 1 + 2  + "1" + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, nmu2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
