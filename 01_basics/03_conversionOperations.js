cb771b5e944310f4c4838e410c506bc35fbc34fa
Aditya Powale <119852907+Aytida26@users.noreply.github.com>
2024-06-19 07:45:57 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,42 @@
+// let score = "33abc"; //NaN
+// let score = null;  // 0
+// let score = undefined;  //NaN
+// let score = true;  //1
+// let score = false;  //0
+let score = "Aditya";  //NaN
+
+
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 , false => 0
+// null => 0
+// undefined => NaN
+
+let isLoggedIn = "abc" ;
+
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+
+// 1 => true , 0 => false
+// "" => false , "abc" => true
+
+
+let someNumber = 24
+console.log(typeof someNumber);
+
+let stringNumber = String(someNumber)
+
+console.log(typeof stringNumber);
+console.log(stringNumber);
