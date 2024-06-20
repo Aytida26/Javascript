6f0a9eb91465da4c6650b2c75c8bcbb7e8020aae
Aditya Powale <119852907+Aytida26@users.noreply.github.com>
2024-06-20 06:22:30 +0000

    comparisions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,28 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(null != 0);
+
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+
+// ==
+console.log("34" == 34); //checks the number and not data type
+
+// ===
+console.log("34" === 34); //checks data type also
